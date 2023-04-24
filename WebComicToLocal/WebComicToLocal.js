63130743d19aee68fc8fcabe071df19990dc46c4
Fernando Ouviña <fernandoouvina07@gmail.com>
2023-04-24 19:59:59 -0300

    Script for fetching comics and writing them into individual .JSON

---
+++ WebComicToLocal/WebComicToLocal.js
@@ -0,0 +1,22 @@
+import fs from "fs-extra";
+import axios from "axios";
+
+const initialID = 2700;
+const maxId = 2701;
+
+for (let i = initialID; i < maxId; i++) {
+  const url = `https://xkcd.com/${i}/info.0.json`;
+  const { data } = await axios.get(url);
+  const { num: id, news, transcript, ...restOfComic } = data;
+
+  const comic = {
+    id,
+    news,
+    transcript,
+    ...restOfComic,
+  };
+
+  const filePath = `./comics/${id}.json`;
+  await fs.outputJSON(filePath, comic);
+  console.log(data);
+}
