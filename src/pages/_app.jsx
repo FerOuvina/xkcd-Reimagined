f53b4d72f27bcb5054f4c9e40e400ca43248ad08
Fernando Ouviña <fernandoouvina07@gmail.com>
2023-04-25 18:28:16 -0300

    "Trying" to set NextUI themes

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,10 +1,25 @@
-import { NextUIProvider} from "@nextui-org/react";
+import { NextUIProvider, createTheme } from "@nextui-org/react";
 import { ThemeProvider } from "next-themes";
-import NavigationBar from "./components/Navbar";
+import { NavigationBar } from "../components/Navbar";
+
+const darkTheme = createTheme({
+  type: "dark",
+});
+
+const lightTheme = createTheme({
+  type: "light",
+});
 
 export default function App({ Component, pageProps }) {
   return (
-    <ThemeProvider>
+    <ThemeProvider
+      defaultTheme="system"
+      attribute="class"
+      value={{
+        light: lightTheme.className,
+        dark: darkTheme.className,
+      }}
+    >
       <NextUIProvider>
         <NavigationBar />
         <Component {...pageProps} />
