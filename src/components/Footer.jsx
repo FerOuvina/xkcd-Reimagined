bbd64b662151ddb6817ee389e7bcd5c51416939c
Fernando Ouviña <fernandoouvina07@gmail.com>
2023-05-02 20:33:48 -0300

    Styling the component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,19 +1,35 @@
-import { Text } from "@nextui-org/react";
+import { Container, Text } from "@nextui-org/react";
 import Link from "next/link";
 
 export function Footer() {
   return (
     <>
-      <footer className="footer">
-        <Text className="footer__copyright">
-          Comics by <Link href="https://xkcd.com/">xkcd</Link>
+      <Container
+        as="footer"
+        display="flex"
+        justify="space-evenly"
+        css={{
+          marginTop: "2rem",
+        }}
+      >
+        <Text h6>
+          Comics by{" "}
+          <Link href="https://xkcd.com/" target="_blank" rel="noreferrer">
+            xkcd
+          </Link>
         </Text>
 
-        <Text>
+        <Text h6>
           Website by{" "}
-          <Link href="https://github.com/FerOuvina">Fernando Ouvi&#241;a</Link>
+          <Link
+            href="https://github.com/FerOuvina"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Fernando Ouvi&#241;a
+          </Link>
         </Text>
-      </footer>
+      </Container>
     </>
   );
 }
